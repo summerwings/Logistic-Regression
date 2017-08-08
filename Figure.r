1c318aade6f4b0644d889873f91e552340bb072c
SUMMER ZHANG(张夏天) <summerwings@outlook.com>
2017-08-08 18:19:15 +0800

    Update Figure.r

--- Figure.r
+++ Figure.r
@@ -1,8 +1,8 @@
 #Sigmoid function 
-x <- seq(from = -10, to = 10, by = 0.01)
-y = exp(x)/(1+exp(x))
+z <- seq(from = -10, to = 10, by = 0.01)
+y = 1/(1+exp(-z))
 
 library(ggplot2)
 
-p <- ggplot(data = NULL, mapping = aes(x = x,y = y))
+p <- ggplot(data = NULL, mapping = aes(x = z,y = y))
 p + geom_line(colour = 'blue')
