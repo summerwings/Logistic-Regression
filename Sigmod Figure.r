3d31f697e51b34df31739778812c3a7f57b7c972
SUMMER ZHANG(张夏天) <summerwings@outlook.com>
2017-08-10 02:28:22 +0800

    Update Sigmod Figure.r

--- Sigmod Figure.r
+++ Sigmod Figure.r
@@ -1,10 +1,14 @@
-#Sigmoid function and Linear function 
+library(ggplot2)
+x <- seq(from = -10, to = 10, by = 0.01)
+#standard Sigmoid function
+g = 1/(1+exp(-x))
+p0 <- ggplot(data = NULL, mapping = aes(x = x,y = g))
+p0 <- p0 + geom_line(colour = 'blue')
+
+#transform Linear function to Sigmoid function
 x <- seq(from = -10, to = 10, by = 0.01)
 y = 2*x + 13
 g = 1/(1+exp(-y))
-
-library(ggplot2)
-
 
 p1 <- ggplot(data = NULL, mapping = aes(x = x,y = y))
 p1 <- p1 + geom_line(colour = 'blue')
